1ef96585134c41e5b2bdc6b997c0b9f67f7fefee
ImsuperSir <imsupersir@gmail.com>
2024-01-04 22:06:13 -0800

    CreatingCRUDRestAPI

---
+++ ImSuperSir.GameStore.API/Entities/Game.cs
@@ -0,0 +1,18 @@
+﻿namespace ImSuperSir.GameStore.API.Entities
+{
+    public class Game
+    {
+        public int Id { get; set; }
+
+        public required string Name { get; set; } //= string.Empty;
+
+        public required string Genre { get; set; }
+
+        public decimal Price { get; set; }
+
+        public DateTime ReleaseDate { get; set; }
+
+        public required string ImageUri { get; set; }
+
+    }
+}
